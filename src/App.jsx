a9751a65abfb672604082ece6b68611ed4f3b6d6
TiaTiu <sparklingsnowflakes_on_my_tiara@yahoo.com>
2026-08-16 20:27:17 +0700

    Add individual participant spending breakdown to summary

--- src/App.jsx
+++ src/App.jsx
@@ -826,7 +826,7 @@ export default function App() {
                   )}
                 </div>
               </div>
-            </div>
+            </>
           )}
         </div>
       </main>
